f74941231e93adeabf69b54a193aaf763a5f091b
Naotsuki82 <147086167+Naotsuki82@users.noreply.github.com>
2025-12-03 03:34:14 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,9 +10,12 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        // TRUST ALL PROXIES
+        // This tells Laravel to trust the headers sent by Railway/Replit
+        // allowing it to see that the site is actually running on HTTPS.
+        $middleware->trustProxies(at: '*');
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
